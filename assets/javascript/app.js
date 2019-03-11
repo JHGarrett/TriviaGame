85b8f6f97dce27cf7111df37800f58aad621fe16
John Garrett <infantrygarrett@icloud.com>
2019-03-10 20:22:32 -0500

    started working on some of the JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,75 @@
+//  variable to store the questions and answer objects
+
+var question = [{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+},
+{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+},
+{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+},
+{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+},
+{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+},
+{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+},
+{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+},
+{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+},
+{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+},
+{
+    quest: "",
+    ans: [""],
+    name: "",
+    correct: "",
+    divClass: "."
+}
+] 
+// end of questions
+
