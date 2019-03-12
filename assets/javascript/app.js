e8e2808cd8d12b7f5fdecb76cf565e72e5a23c0e
John Garrett <infantrygarrett@icloud.com>
2019-03-11 21:25:21 -0500

    added the display question function to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -84,3 +84,19 @@ var startGame = $("#startBtn").on("click", function(){
 });
 // end of start game function
     
+var questionDisplay = function(){
+    $(".questions :not('#submitQuiz')").empty();
+    // $(".questions").empty();
+    for (var i = 0; i < 10; i ++){
+        $(".questions").append("<div class='" + questions[i].name + "'></div");
+        $(questions[i].divClass).append('<div class ="ques-title">' + question[i].ques + '</div>');
+        for (var j = 0; j <= 3; j++){
+            $(questions[i].divClass).append('<input type="radio" name+"' + questions[i].name + '" value+"' + questions[i].ans[j] + '"/><label for="' + labels[j] + '">' + questions[i].ans[j] + '</label>');
+        } 
+        // end for statement
+        $(".questions")/append("<hr />");  
+    }
+}
+// end function question display
+
+
